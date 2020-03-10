fa75c27823b66219a06cfc793525e1012903d6af
Nikolaj Rønne <nikolajron@gmail.com>
2020-03-10 15:26:26 +0100

    lecture

---
+++ lectures/latex/exp.cs
@@ -0,0 +1,13 @@
+using System;
+public static class nonef{
+public static double exp(double x){
+	if(x<0)return 1/exp(-x);
+	if(x>4){double r=exp(x/4); return r*r*r*r;}
+	if(x>2){double r=exp(x/2); return r*r;}
+	Func<double,vector,vector> expeq=(t,y)=>new vector(y[0]);
+	double a=0;
+	vector ya=new vector(1.0);
+	vector res=ode.rk23(expeq,a,ya,x,acc:1e-3,eps:1e-3);
+	return res[0];
+	}
+}
---
+++ lectures/latex/main.cs
@@ -0,0 +1,7 @@
+using static System.Console;
+class main{
+static void Main(){
+	for(double x=-2;x<=5;x+=0.125)
+		WriteLine($"{x} {nonef.exp(x)}");
+}
+}
