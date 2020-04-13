b75e3a827834b8ed7bb2955674c57c50b84bbf8a
Nikolaj Rønne <nikolajron@gmail.com>
2020-04-13 10:09:17 +0200

    working with rk12

--- problems/ode/main.cs
+++ problems/ode/main.cs
@@ -7,7 +7,8 @@ class main {
 	Func<double, vector, vector> f = delegate(double x, vector y) {
 	    return new vector(y[1], -y[0]);
 	};
-	rk sin = new rk(f, 0, 2, new vector(1, 0));
-	(sin.yb).print("sin(2)=");
+	rk sincos = new rk(f, 0, 1, new vector(0, 1), initStep:1.0);
+	Write($"sin(1) = {sincos.yb[0]}\n");
+	Write($"cos(1) = {sincos.yb[1]}\n");
     }
 }
--- problems/ode/rk.cs
+++ problems/ode/rk.cs
@@ -1,5 +1,6 @@
 using System;
 using static System.Math;
+using static System.Console;
 using System.Collections.Generic;
 
 public class rk {
@@ -10,10 +11,14 @@ public class rk {
     static double acc; // absolute accuracy goal
     static double err; // relative accuracy goal
     static double h; // initial step size
+    static int nMax;
     public vector yb; // integrated result at y(b)
-    
+    static public List<double> ts; // list to store intermediate t values
+    static public List<vector> ys; // list to store intermediate f(t) valyes
+        
     public rk(Func<double, vector, vector> func, double startPoint, double endPoint, vector startValue,
-	      double absAcc=10e-3, double relAcc=10e-3, double initStep=0.01){
+	      bool storeVal=false, double absAcc=10e-3, double relAcc=10e-3,
+	      double initStep=0.1, int limit=999){
 	f = func;
 	a = startPoint;
 	b = endPoint;
@@ -21,15 +26,16 @@ public class rk {
 	acc = absAcc;
 	err = relAcc;
 	h = initStep;
-
+	nMax = limit;
+	if (storeVal) {
+	    ts = new List<double>();
+	    ys = new List<vector>();
+	}
 	yb = driver(); 
     }
 
-    static vector yh; // y(t+h): output from step
-    static vector dy; // error estimate: output from step
-    
-    static void rkstep12(double t, vector yt, double h) {
-	matrix A = new matrix("0.0,0.0;1.0,0.0"); // Buthers tableau
+    static vector[] rkstep12(double t, vector yt, double h) {
+	matrix A = new matrix("0,0;1,0"); // Buthers tableau
 	vector cs = new vector(0.0, 1.0); // nodes
 	vector bs = new vector(0.5, 0.5); // weights
 	vector bss = new vector(1.0, 0.0); // weights one order lower that bs
@@ -39,19 +45,21 @@ public class rk {
 	for (int i = 0; i<yt.size; i++) {
 	    ka[i] = 0;
 	}
-	for (int r = 0; r<bs.size; r++) {
-	    for (int c = 0; c<r; c++){
-		ks[r] = f(t + cs[r]*h, yt + h*ka);
-		
-		for (int i = 0; i<yt.size; i++) {
-		    ka[i] = 0;
-		}
-		for (int j=0; j<r; j++){
-		    ka += A[r, j]*ks[r];
+	int r = 0;
+	while (r<bs.size) {
+	    ks[r] = f(t + cs[r]*h, yt + h*ka);
+
+	    for (int i = 0; i<yt.size; i++) {
+		ka[i] = 0;
+	    }
+	    r++;
+	    if (r<bs.size){
+		for (int c = 0; c<(r); c++){
+		    ka += A[r, c]*ks[c];
 		}
 	    }
 	}
-	yh = yt;
+	vector yh = yt;
 	yTilde = yt;
 	for (int i = 0; i<bs.size; i++) {
 	    double bi = bs[i];
@@ -59,22 +67,56 @@ public class rk {
 	    yh += h*bi * ks[i];
 	    yTilde += h*bsi*ks[i];
 	}
-	dy = yh - yTilde; // error estimate for step
+	vector dy = yh - yTilde; // error estimate for step
+	vector[] result = {yh, dy};
+	return result;
     }
 
     static vector driver() {
 	double t = a;
 	vector yt = ya;
-	double e, tol;
+	vector yh, dy;
+	double e, tol, hOld;
+	int s, nSteps=0;
+	if(ts!=null) {
+	    ts.Clear();
+	    ts.Add(t);
+	}
+	if(ys!=null) {
+	    ys.Clear();
+	    ys.Add(yt);
+	}
+
 	while(t < b){
+	    s=0;
+	    if (nSteps > nMax) {
+		Error.Write("To many steps taken! \n");
+		return yt;
+	    }
 	    do{
-	    rkstep12(t, yt, h);
-	    tol = (acc+err*yt.norm())*Sqrt(h/(b-a));
-	    e = dy.norm();
-	    h = h*Pow(tol/e, 0.25)*0.95;
-	    }while(e>=tol);
+		if(t+h>b) {
+		    h=b-t;
+		}
+		vector[] trialStep = rkstep12(t, yt, h);
+		yh = trialStep[0];
+		dy = trialStep[1];
+		tol = (acc+err*yt.norm())*Sqrt(h/(b-a));
+		e = dy.norm();
+		hOld = h;
+		h = h*Pow(tol/e, 0.25)*0.95;
+		s++;
+		
+	    }while(e>tol);
+	    Error.Write("Step taken with step size: {0} \n Number of bad steps: {1} \n", hOld, s-1);
 	    t += h;
-	    yt += yh; 
+	    yt = yh;
+	    if(ts!=null) {
+		ts.Add(t);
+	    }
+	    if(ys!=null) {
+		ys.Add(yt);
+	    }
+	    nSteps++;
 	}
 	return yt;
     }//driver
