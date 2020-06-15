099585d1ece98fb9e9f3fe039a40c87e70926c64
Nikolaj Rønne <nikolajron@gmail.com>
2020-06-15 10:56:21 +0200

    exam done

--- exam/main.cs
+++ exam/main.cs
@@ -7,30 +7,44 @@ class main {
     public static void Main() {
 	matrix A = rndMat.randomMatrix(5, 3);
 	Write("Example of Golub-Kahan-Lanczos bidiagonalization on tall matrix:\n");
+	Write("The matrix A is written as A = UBV^T. Example for random A given as:\n");
 	A.print("A =");
 	var gkl = new bidiag(A);
+	Write("The found matrix U and V are: \n");
 	gkl.U.print("\n U = ");
 	gkl.V.print("\n V = ");
 
 	matrix B_calc = gkl.U.transpose() * A * gkl.V;
-	B_calc.print("\n U^T*A*V = ");
+	Write("The bidiogonal matrix calculated via U and V, and directly from the method is given below: \n");
+	B_calc.print("\n U^T * A * V = ");
 	gkl.B.print("\n B = ");
 
+	Write("The difference between A and the representation of A through B:\n");
 	(A-gkl.U*gkl.B*gkl.V.transpose()).print("\n 0 = A - U*B*V^T =");
 
-	Write("\n Use bidiagonalization to solve Ax=b equation:\n");
-	A = new matrix("1,1,1;0,2,5;2,5,-1");
-	A.print("A");
-	vector b = new vector("6,-4,27");
-	b.print("b=");
+	Write("\n Use bidiagonalization to solve Ax=b equation for square random matrix matrix A:\n");
+	A = rndMat.randomMatrix(3,3);
+	A.print("A = ");
+	Write("With random vector b:\n");
+	vector b = rndMat.randomVector(3);
+	b.print("b =");
 	gkl = new bidiag(A);
-	gkl.solver(b).print("x=");
+	vector x = gkl.solver(b);
+	Write("Solution to equation:\n");
+	x.print("x =");
+	vector diff = A*x-b;
+	Write("Check if solution is correct: \n");
+	diff.print("0 = A*x - b =");
+	
+
+	Write("\n Determinant (found as product of diagonal of B) and inverse:\n");
 	Write($"|Det(A)| = {gkl.determinant()}\n");
 	gkl.inverse().print("A^-1 = ");
-	(gkl.inverse()*A).print("A^-1 * A = ");
+	Write("Check if inverse is correct: \n");
+	(gkl.inverse()*A).print("1 = A^-1 * A = ");
 
 	int success = 0;
-	double eps = 1e-8;
+	double eps = 1e-7; 
 	success = tests.differenceTest(eps);
 	success += tests.solverTest(eps);
 	success += tests.inverseTest(eps);
