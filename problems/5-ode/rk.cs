a5faa04645066ab1157baec24ad5ff0a2e7ebd39
Nikolaj Rønne <nikolajron@gmail.com>
2020-06-17 20:26:39 +0200

    analytic sin/cos on plot

--- problems/5-ode/rk.cs
+++ problems/5-ode/rk.cs
@@ -17,7 +17,7 @@ public class rk {
     static public List<vector> ys; // list to store intermediate f(t) valyes
         
     public rk(Func<double, vector, vector> func, double startPoint, double endPoint, vector startValue,
-	      bool storeVal=false, double absAcc=10e-3, double relAcc=10e-3,
+	      bool storeVal=false, double absAcc=1e-2, double relAcc=1e-2,
 	      double initStep=0.1, int limit=999){
 	f = func;
 	a = startPoint;
@@ -34,6 +34,7 @@ public class rk {
 	yb = driver(); 
     }
 
+        
     static vector[] rkstep12(double t, vector yt, double h) {
 	matrix A = new matrix("0,0;1,0"); // Buthers tableau
 	vector cs = new vector(0.0, 1.0); // nodes
@@ -71,7 +72,7 @@ public class rk {
 	vector[] result = {yh, dy};
 	return result;
     }
-
+    
     static vector driver() {
 	double t = a;
 	vector yt = ya;
@@ -120,4 +121,53 @@ public class rk {
 	}
 	return yt;
     }//driver
+
+    /*
+      rk45
+      matrix A = new matrix($"0,0,0,0,0,0;0.25,0,0,0,0,0;{3.0/32},{9.0/32},0,0,0,0;{1932.0/2197},{-7200.0/2197},{7296.0/2197},0,0,0;{439.0/216},{-8},{3680.0/513},{-845/4104},0,0;{-8.0/27},2,{-3544.0/2565},{1859.0/4104},{-11.0/40},0"); // Buthers tableau
+      vector cs = new vector(new double[] {0.0, 0.25, 3.0/8, 12/13, 1, 0.5}); // nodes
+      vector bs = new vector(new double[] {16.0/135, 0, 6656.0/12825, 28561.0/56430, -9.0/50, 2.0/55}); // weights
+      vector bss = new vector(new double[] {25.0/216, 0, 1408.0/2565, 2197.0/4104, -1.0/5, 0}); // weights one order lower that bs
+     */
+    /*
+
+    static vector[] rkstep23(double t, vector yt, double h) {
+	matrix A = new matrix($"0,0,0,0;0.5,0,0,0;0,{3.0/4},0,0;{2.0/9},{1.0/3},{4.0/9},0"); // Buthers tableau
+	vector cs = new vector(new double[] {0.0, 0.5, 3.0/4, 1}); // nodes
+	vector bs = new vector(new double[] {2.0/9, 1.0/3, 4.0/9, 0}); // weights
+	vector bss = new vector(new double[] {7.0/24, 1.0/4, 1.0/3, 1.0/8}); // weights one order lower that bs
+	vector yTilde; // one lower order output
+	matrix ks = new matrix(yt.size, bs.size); // matrix with functions calls as colomns
+	vector ka = new vector(yt.size); // vector of matrix elements times previous ks
+	for (int i = 0; i<yt.size; i++) {
+	    ka[i] = 0;
+	}
+	int r = 0;
+	while (r<bs.size) {
+	    ks[r] = f(t + cs[r]*h, yt + h*ka);
+
+	    for (int i = 0; i<yt.size; i++) {
+		ka[i] = 0;
+	    }
+	    r++;
+	    if (r<bs.size){
+		for (int c = 0; c<(r); c++){
+		    ka += A[r, c]*ks[c];
+		}
+	    }
+	}
+	vector yh = yt;
+	yTilde = yt;
+	for (int i = 0; i<bs.size; i++) {
+	    double bi = bs[i];
+	    double bsi = bss[i];
+	    yh += h*bi * ks[i];
+	    yTilde += h*bsi*ks[i];
+	}
+	vector dy = yh - yTilde; // error estimate for step
+	vector[] result = {yh, dy};
+	return result;
+    }
+    */
+    
 }//rk
